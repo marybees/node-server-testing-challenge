e908c866b794a5ffa48e156bf7c262e968809e32
Mary Baker <meb601@gmail.com>
2020-10-15 15:32:59 -0400

    Create skeleton file structure, add server and basic tests

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const Dogs = require("../dogs/dogsModel.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "is up and running on `${PORT}`" });
+});
+
+// returns http 200
+// returns json
+// the body has an api propety and the value is "is up"
+
+server.get("/dogs", (req, res) => {
+  Dogs.getAll()
+    .then((dogs) => {
+      res.status(200).json(dogs);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = server;
